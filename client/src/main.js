e291d23a123bf51d6c4f31222de2c33830079cb8
Jj <Jjzettler@gmail.com>
2024-08-24 17:45:31 -0500

    minor css changes and addition of service. Routing onBefore reroute addition

--- client/src/main.js
+++ client/src/main.js
@@ -9,7 +9,17 @@ const appName = import.meta.env.VITE_APP_NAME || 'ILYTAT'
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title || appName // Set the title from the route meta or use a default title
+
+  // Check that the route exists, otherwise redirect to the 404 page
+  if (!to.matched.length) {
+    next({ name: 'NotFound' })
+  }
   next()
 })
+
+router.onError((error, to, from) => {
+  const errorMessage = `There was an error loading the page ${ to.name } from ${ from.name }: ${ error.message }`
+  console.error(errorMessage, error)
+});
 
 app.use(createPinia()).use(router).mount('#app')
