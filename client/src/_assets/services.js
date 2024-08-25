af1d5a65b51749843e2bb7ff9be1db7e69d076bf
Jjzme2 <jjzettler@gmail.com>
2024-08-25 17:55:30 -0500

    moderate css changes emailService update addition of new services

--- client/src/_assets/services.js
+++ client/src/_assets/services.js
@@ -1,57 +1,65 @@
+const categories = {
+  general: 'General Services',
+  recurring: 'Recurring Services',
+  seasonal: 'Seasonal Services',
+  specialty: 'Specialty Services',
+  other: 'Other Services'
+}
+
 const services = [
   {
-    serviceName: 'Mowing & Edging',
+    serviceName: 'Mulch Installation & Bed Edging',
     description:
-      'We offer mowing and edging services for residential and commercial properties. Our team will keep your lawn looking great!',
-    category: 'Seasonal Services,',
-    price: 30,
-    image: 'mowing.png',
-    notes: 'Additional charges may apply for larger properties and further distances.'
+      'Enhance your garden beds with our mulch installation and bed edging services. We help retain moisture and improve the overall aesthetics of your landscape, ensuring your outdoor spaces look their best.',
+    category: categories.seasonal,
+    price: 125,
+    image: 'mulch.png',
+    notes: 'Pricing is per cubic yard of mulch installed.'
   },
   {
-    serviceName: 'Shoveling & Snow Removal',
+    serviceName: 'Seasonal Clean-Up',
     description:
-      'We offer shoveling and snow removal services for residential and commercial properties. Our team will keep your property safe and accessible!',
-    category: 'Seasonal Services,',
-    price: 50,
-    image: 'shoveling.png',
-    notes: 'Additional charges may apply for larger properties and further distances.'
+      'Prepare your property for the changing seasons with our comprehensive seasonal clean-up services. Our team will remove leaves, debris, and other unwanted materials from your outdoor spaces, ensuring a clean and tidy appearance.',
+    category: categories.seasonal,
+    price: 100,
+    image: 'clean_up.png',
+    notes: 'Includes lawn mowing, shovel snow, and other seasonal tasks.'
   },
   {
-    serviceName: 'Grocery Delivery',
+    serviceName: 'Grocery Delivery - Recurring',
     description:
-      'We offer grocery delivery services for those who are unable to shop for themselves. Our team will deliver your groceries to your door! Feel comfortable knowing who is delivering your goods.',
-    category: 'Recurring Services,',
+      'Enjoy the convenience of our grocery delivery services. We shop for your groceries and deliver them to your doorstep on a recurring basis, saving you time and hassle. Accept deliveries comfortably, knowing that your groceries are in good hands.',
+    category: categories.recurring,
     price: 100,
     image: 'grocery_recurring.png',
-    notes:
-      'Additional charges may apply for larger orders and further distances. This is considered a monthly expense up to 4 times a month.'
+    notes: ''
   },
   {
-    serviceName: 'Grocery Delivery (One Time)',
-    description: 'We offer grocery delivery services for those who are unable to shop for themselves. Our team will deliver your groceries to your door! Feel comfortable knowing who is delivering your goods.',
-    category: 'General Services,',
-    price: 30,
+    serviceName: 'Grocery Delivery - One-Time',
+    description:
+      "Need groceries but don't have the time to shop? Let us handle it! Our team will shop for your groceries and deliver them to your doorstep, ensuring you have everything you need without the hassle of going to the store.",
+    category: categories.general,
+    price: 25,
     image: 'grocery.png',
-    notes: 'Additional charges may apply for larger orders and further distances.'
-  },
-]
-
-const categories = [
-  {
-    name: 'General Services'
+    notes: 'Prices may vary based on the total of the order.'
   },
   {
-    name: 'Recurring Services'
+    serviceName: 'Interior Car Detailing',
+    description:
+      'Keep your vehicle spotless with our interior car detailing services. Our team thoroughly cleans your vehicle inside and out, providing a fresh and tidy environment.',
+    category: categories.general,
+    price: 50,
+    image: 'car_detailing_interior.png',
+    notes: ''
   },
   {
-    name: 'Seasonal Services'
-  },
-  {
-    name: 'Specialty Services'
-  },
-  {
-    name: 'Other Services'
+    serviceName: 'Exterior Car Detailing',
+    description:
+      "Maintain the exterior of your vehicle with our professional detailing services. We clean, polish, and protect your car's exterior, ensuring a shiny and well-maintained appearance.",
+    category: categories.general,
+    price: 50,
+    image: 'car_detailing_exterior.png',
+    notes: ''
   }
 ]
 
---
+++ client/src/components/app/mainElements/cards/Content_Cards/ServiceCard.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="service-card">
+    <div class="service-info">
+      <img class="service-icon" :src="imagePath" />
+      <h2>{{ service.serviceName }}</h2>
+      <p>{{ truncatedDescription }}</p>
+      <p class="price">${{ service.price }}</p>
+    </div>
+    <div class="hover-info">
+      <p>{{ service.description }}</p>
+      <p v-if="service.notes">{{ service.notes }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ServiceCard',
+  props: {
+    service: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    truncatedDescription() {
+      const maxLength = 25 // Set the character limit for the description
+      return this.service.description.length > maxLength
+        ? this.service.description.substring(0, maxLength) + '...'
+        : this.service.description
+    },
+    imagePath() {
+      const path = `/images/app/full/${this.service.image}`
+      console.log(path)
+      return path
+    }
+  }
+}
+</script>
+
+<style scoped>
+.service-card {
+  position: relative;
+  width: 300px;
+  height: 400px;
+  background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+  border-radius: 15px;
+  overflow: hidden;
+  transition:
+    transform 0.3s,
+    box-shadow 0.3s;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 3rem auto;
+}
+
+.service-card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.service-info {
+  position: absolute;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+  width: 100%;
+  padding: 1rem;
+  box-sizing: border-box;
+  backdrop-filter: blur(10px);
+}
+
+.service-info h2 {
+  margin: 0;
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+.service-info p {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.price {
+  font-weight: bold;
+  font-size: 1.2rem;
+  color: #ffcc00;
+}
+
+.hover-info {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.9);
+  color: white;
+  padding: 1rem;
+  box-sizing: border-box;
+  opacity: 0;
+  transition: opacity 0.3s;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.service-card:hover .hover-info {
+  opacity: 1;
+}
+
+.service-icon {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-bottom: 1rem;
+  object-fit: cover;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+/* Responsive adjustments */
+@media screen and (max-width: 768px) {
+  .service-card {
+    max-width: 100%; /* Full width on small screens */
+    margin: 1rem auto; /* Adjust margin for smaller screens */
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .service-card {
+    max-width: 200px; /* Full width on very small screens */
+    margin: 0.5rem auto; /* Adjust margin for very small screens */
+  }
+}
+</style>
